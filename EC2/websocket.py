4e3d2f8e289ae996de8e6dd636cc67df8ed0268a
michael rokitko <alarn777@gmail.com>
2020-06-16 14:37:47 +0300

    Changes to websocket on EC2

--- EC2/websocket.py
+++ EC2/websocket.py
@@ -1,14 +1,10 @@
 import asyncio
+import sys
+import time
 import websockets
-import boto3
-import json
-from io import BytesIO
-import json
 import boto3
 import cv2
 import tempfile
-import h5py
-import simplejson
 from tensorflow.keras.models import model_from_json
 
 s3 = boto3.resource('s3')
@@ -21,11 +17,8 @@ def loadModel(img):
     json_file = open('new_model.json', 'r')
     loaded_model_json = json_file.read()
     loaded_model = model_from_json(loaded_model_json)
-    bucket = 'planty-lambda-functions'
-    key = 'ímage-processing/new_model.json'
     loaded_model.load_weights("new_model.h5")
     print("Loaded model from disk")
-    model_weights = 'image-processing/new_model.h5'
     loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     # predict on pic
     IMG_SIZE = 256
@@ -39,13 +32,12 @@ def loadModel(img):
 
     print(predicitions)
     print(CATEGORIES[int(predicitions[0][0])])
-    #    return float(predicitions[0][0])
-
     return CATEGORIES[int(predicitions[0][0])]
 
 
 def checkPicture(data):
     print("Checking...")
+    print(data)
 
     bucket = 'pictures-bucket-planty165521-planty'
     bucket = s3.Bucket(bucket)
@@ -56,35 +48,37 @@ def checkPicture(data):
     except:
         print('Error testing the file')
         return 'Error'
-    
+
     res = []
     try:
-    	with open(tmp.name, 'wb') as f:
-                my_object.download_fileobj(f)
-                res = loadModel(tmp.name)
-                
-                return data + ';' + res
+        with open(tmp.name, 'wb') as f:
+            my_object.download_fileobj(f)
+            print(tmp.name)
+            res = loadModel(tmp.name)
+
+            return data + ';' + res
     except:
         print('Error testing the file')
         return 'Error'
+
 
 def on_message(message):
     command = message.split(";")
     print(command)
     if command[0] == "FROM_EC2":
-        return "Ignore",""
+        return "Ignore", ""
 
     elif command[2] == "CHECK_IMAGE":
         res = checkPicture(command[3])
-        return str(res),""
-    
+        return str(res), ""
+
     elif command[2] == "CHECK_IMAGE_RAND":
         res = checkPicture(command[3])
-        return str(res),"RAND"
+        return str(res), "RAND"
 
     else:
         print("Unknown Command: {0}".format(command))
-        return "FAILED" , ""
+        return "FAILED", ""
 
 
 async def websocket_handler():
@@ -102,7 +96,7 @@ async def websocket_handler():
                 answer = '{{\"action":"message","message":"FROM_EC2;EC2;BAD_COMMAND"}}'
                 await websocket.send(answer)
                 continue
-            result,mode = on_message(message)
+            result, mode = on_message(message)
             if result == "Ignore":
                 print('Ignore')
                 continue
@@ -110,40 +104,27 @@ async def websocket_handler():
             if result == 'FAILED':
                 print('FAILED')
                 continue
-            
+
             answer = f'{{\"action":"message","message":"FROM_EC2;EC2;IMAGE_STATUS;{result}"}}'
             if mode == "RAND":
                 answer = f'{{\"action":"message","message":"FROM_EC2;EC2;IMAGE_STATUS_RAND;{result}"}}'
-        
+
             await websocket.send(answer)
             print(f'>>> {result}')
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            asyncio.get_event_loop().run_until_complete(websocket_handler())
-        except websockets.exceptions.ConnectionClosedOK:
-            print("Connection closed by server.\n Reconnecting.\n")
-        except:
-            raise
-        finally:
-            pass
-
-if __name__ == "__main__":
-
     while True and retryCounter < 20:
         try:
             asyncio.get_event_loop().run_until_complete(websocket_handler())
         except websockets.exceptions.ConnectionClosedOK:
-            print("Connection closed by server.Reconnecting. Retry:" + retryCounter)
-            retryCounter = retryCounter+1
+            print("Connection closed by server.Reconnecting. Retry:" + str(retryCounter))
+            retryCounter = retryCounter + 1
 
         except websockets.exceptions.ConnectionClosedError:
-            print("Connection closed by server error. Reconnecting. Retry:" + retryCounter)
+            print("Connection closed by server error. Reconnecting. Retry:" + str(retryCounter))
             time.sleep(15)
-            retryCounter = retryCounter+1
+            retryCounter = retryCounter + 1
         except:
             pass
             raise
-        
